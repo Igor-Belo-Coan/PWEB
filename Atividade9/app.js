7ef74b4a6ad18ed1931fae5173556b0834e0ca6f
Igor Belo Coan <igorbelocoan3@gmail.com>
2023-10-17 16:56:36 -0300

    Atividade 9
    Incompleto

---
+++ Atividade9/app.js
@@ -0,0 +1,28 @@
+var dados = [];
+
+document.getElementById('enviar').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let pessoa = {
+        idade: document.getElementById('idade').value,
+        sexo: document.getElementById('sexo').value,
+        opiniao: document.getElementById('opiniao').value
+    }
+
+    console.log(pessoa.idade)
+    console.log(pessoa.sexo)
+    console.log(pessoa.opiniao)
+
+    dados.push(pessoa);
+
+});
+
+document.getElementById('relatorio').addEventListener('click', () => {
+
+    console.log(dados);
+    console.log(pessoa);
+
+});
+
+
+
